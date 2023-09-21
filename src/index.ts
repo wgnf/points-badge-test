f9345ff48d49a99c5ccd61f3fa381a3977ee030c
Martin Wagenführ <martin@wgnf.de>
2023-09-21 14:17:50 +0200

    introduce default-case and remove now unnecessary local variable
    this makes the whole function easier to read and grasp, as the default-case of the function, is actually represented by the default-case of the switch-statement

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,14 @@ import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
 
 export const getUsersBadge = ( user: User ): Icon => {
-  let badge = Icon.DEFAULT;
   switch ( true ) {
     case ( user.solutionCount >= 5 && user.solutionCount < 25 ):
-      badge = Icon.BADGE_BRONZE;
-      break;
+      return Icon.BADGE_BRONZE;
     case ( user.solutionCount >= 25 && user.solutionCount < 50 ):
-      badge = Icon.BADGE_SILVER;
-      break;
+      return Icon.BADGE_SILVER;
     case ( user.solutionCount >= 50 ):
-      badge = Icon.BADGE_GOLD;
-      break;
-  }
-  return badge;
+      return Icon.BADGE_GOLD;
+    default:
+      return Icon.DEFAULT;
+    }
 };
